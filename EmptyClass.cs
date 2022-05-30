e926e1ff20ff0d9f9e747817ba8dd8a3608cbac4
andreagoodkid2 <100676785+andreagoodkid2@users.noreply.github.com>
2022-05-30 15:30:46 +0700

    Add files via upload

---
+++ EmptyClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+public class congtyxaydung
+{
+    public static void Main(string[] args)
+    {
+
+        
+        thicong[] objthicong = new thicong[1];
+        int choice;
+        do
+        {
+            Console.WriteLine("Make a choice from 1 to 3");
+            Console.WriteLine("Press 1: Load data");
+            Console.WriteLine("Press 2: Show all data");
+            Console.WriteLine("Press 3: Exit");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    LoadData(ref objthicong);
+                    break;
+                case 2:
+                    DisplayAllCongtrinh(objthicong);
+                    break;
+                default:
+                    break;
+            }
+            Console.Clear();
+        }
+
+        while (choice != 3);
+
+
+
+    }
+    public static void LoadData(ref thicong[] objthicong)
+    {
+        StreamReader reader = new StreamReader("huyanhdeptrai1.txt");
+
+
+        int size = Convert.ToInt32(reader.ReadLine());
+        objthicong = new thicong[size];
+
+        for (int i = 0; i < objthicong.Length; i++)
+        {
+            objthicong[i] = new thicong();
+            objthicong[i].Congtrinh = reader.ReadLine();
+            objthicong[i].Giaca = Convert.ToInt32(reader.ReadLine());
+            objthicong[i].Thoigian = Convert.ToInt32(reader.ReadLine());
+
+        }
+        
+        
+        reader.Close();
+    }
+    
+
+
+
+
+        public static void DisplayAllCongtrinh(thicong[] objthicong)
+    { 
+        Console.Clear();
+        for (int i = 0; i < objthicong.Length; i++)
+        {
+            objthicong[i].DisplayInformation();
+        }
+        Console.ReadKey();
+
+    }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+
+
+public class thicong
+{
+    private string congtrinh;
+    private int giaca;
+    private int thoigian;
+    
+
+    public string Congtrinh
+    {
+        get { return congtrinh; }
+        set { congtrinh = value; }
+    }
+    public int Giaca
+    {
+        get { return giaca; }
+        set { giaca = value; }
+        
+    }
+    public int Thoigian
+    {
+        get { return thoigian; }
+        set { thoigian = value; }
+    }
+    
+    public void DisplayInformation()
+    {
+        Console.WriteLine("{0} ", Congtrinh);
+        Console.WriteLine("gia ca: {0} VND", Giaca);
+        Console.WriteLine("thoi gian thi cong: {0} ngay", Thoigian);
+    }
+    
+    
+
+    public void DisplayArray(int[] Giaca)
+    {
+        for (int i = 0; i < Giaca.Length; i++)
+        {
+            Console.WriteLine("{0}", Giaca[i]);
+        }
+       
+    }
+    public void BubbleSortArray(int[] Giaca)
+    {
+        bool swap;
+        int temp;
+        do
+        {
+            swap = false;
+            for (int i = 0; i < (Giaca.Length - 1); i++)
+            {
+                if(Giaca[i] > Giaca[i + 1])
+                {
+                    temp = Giaca[i];
+                    Giaca[i] = Giaca[i + 1];
+                    Giaca[i + 1] = temp;
+                    swap = true;
+                }
+            }
+        } while (swap == true);
+    }
+ 
+}
+
+
+
+
+
+
+    
